2404339ebfb29eb91edf24c6b1eb9e7178d0aba4
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-29 12:42:17 -0300

    fixing task service

--- back-end/services/taskService.js
+++ back-end/services/taskService.js
@@ -10,25 +10,26 @@ class TaskService extends BaseService {
   }
 
   async getAll(token) {
-    try {
-      const user = await prisma.user.findFirst({
-        where: { token },
-      });
+  try {
+    const session = await prisma.session.findUnique({
+      where: { token },
+      include: { user: true },
+    });
 
-      if (!user) {
-        throw new Error('User not found or invalid token');
-      }
-
-      const tasks = await this.model.findMany({
-        where: { userId: user.id },
-      });
-
-      return tasks;
-    } catch (e) {
-      logger.logError(e);
-      throw new Error('Error fetching tasks for user');
+    if (!session || !session.isValid || new Date(session.expiresAt) < new Date()) {
+      throw new Error('User not found or invalid/expired token');
     }
+
+    const tasks = await this.model.findMany({
+      where: { userId: session.userId },
+    });
+
+    return tasks;
+  } catch (e) {
+    logger.logError(e);
+    throw new Error('Error fetching tasks for user');
   }
+}
 }
 
 module.exports = TaskService;
