bde75db70ca03b41dc050c6510fb837dbfc0126f
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-30 15:40:42 -0300

    adding status to the db

--- back-end/schemas/taskSchema.js
+++ back-end/schemas/taskSchema.js
@@ -4,7 +4,10 @@ const taskSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
   date: Joi.string().required(),
-  userId: Joi.string()
+  userId: Joi.string(),
+  status: Joi.string()
+  .valid("CONCLUIDO", "EM_ANDAMENTO", "A_FAZER")
+  .required()
 });
 
 module.exports = taskSchema;
