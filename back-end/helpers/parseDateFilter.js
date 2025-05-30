db1392f4e73ed8865e05b888785607cf7a405338
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-05-30 17:42:42 -0300

    adjusting filter format

---
+++ back-end/helpers/parseDateFilter.js
@@ -0,0 +1,23 @@
+function parseDateFilter(input) {
+
+  const fullDateMatch = input.match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+  if (fullDateMatch) {
+    const [, dd, mm, yyyy] = fullDateMatch;
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+  const partialDateMatch = input.match(/^(\d{2})\/(\d{2})$/);
+  if (partialDateMatch) {
+    const [, dd, mm] = partialDateMatch;
+    return `${mm}-${dd}`;
+  }
+
+  const monthYearMatch = input.match(/^(\d{2})\/(\d{4})$/);
+  if (monthYearMatch) {
+    const [, mm, yyyy] = monthYearMatch;
+    return `${yyyy}-${mm}`;
+  }
+  return null;
+}
+
+module.exports = parseDateFilter;
--- back-end/services/taskService.js
+++ back-end/services/taskService.js
@@ -3,43 +3,49 @@ const { PrismaClient } = require('@prisma/client');
 const taskSchema = require('../schemas/taskSchema'); 
 const prisma = new PrismaClient();
 const logger = require('./logService');
+const parseDateFilter = require('../helpers/parseDateFilter');
 
 class TaskService extends BaseService {
   constructor() {
     super(prisma.task, taskSchema);
   }
 
-    async getAll(token, filter, status) {
+  async getAll(token, filter, status) {
     try {
       const session = await prisma.session.findUnique({
         where: { token },
         include: { user: true },
       });
-  
+
       if (!session || !session.isValid || new Date(session.expiresAt) < new Date()) {
         throw new Error('User not found or invalid/expired token');
       }
-  
+
       const where = {
         userId: session.userId,
       };
-  
+
       if (filter) {
+        let dateFilter = parseDateFilter(filter);
         where.OR = [
           { title: { contains: filter, mode: "insensitive" } },
           { description: { contains: filter, mode: "insensitive" } },
-          { date: { contains: filter, mode: "insensitive" } },
         ];
+        if (dateFilter) {
+          where.OR.push({ date: { contains: dateFilter, mode: "insensitive" } });
+        } else {
+          where.OR.push({ date: { contains: filter, mode: "insensitive" } });
+        }
         const statusValues = ["CONCLUIDO", "EM_ANDAMENTO", "A_FAZER"];
         if (statusValues.includes(filter.toUpperCase())) {
           where.OR.push({ status: { equals: filter.toUpperCase() } });
         }
       }
-  
+
       if (status && ["CONCLUIDO", "EM_ANDAMENTO", "A_FAZER"].includes(status.toUpperCase())) {
         where.status = status.toUpperCase();
       }
-  
+
       const tasks = await this.model.findMany({ where });
       return tasks;
     } catch (e) {
