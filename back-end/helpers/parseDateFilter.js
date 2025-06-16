696b82be20fc6f73a94332c6048c65535eca09bc
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-06-15 23:07:27 -0300

    fix(parseDateFilter): fixing the possibility of an unvalid date

--- back-end/helpers/parseDateFilter.js
+++ back-end/helpers/parseDateFilter.js
@@ -3,18 +3,21 @@ function parseDateFilter(input) {
   const fullDateMatch = input.match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
   if (fullDateMatch) {
     const [, dd, mm, yyyy] = fullDateMatch;
+    if (parseInt(mm) < 1 || parseInt(mm) > 12 || parseInt(dd) < 1 || parseInt(dd) > 31) return null;
     return `${yyyy}-${mm}-${dd}`;
   }
 
   const partialDateMatch = input.match(/^(\d{2})\/(\d{2})$/);
   if (partialDateMatch) {
     const [, dd, mm] = partialDateMatch;
+    if (parseInt(mm) < 1 || parseInt(mm) > 12 || parseInt(dd) < 1 || parseInt(dd) > 31) return null;
     return `${mm}-${dd}`;
   }
 
   const monthYearMatch = input.match(/^(\d{2})\/(\d{4})$/);
   if (monthYearMatch) {
     const [, mm, yyyy] = monthYearMatch;
+    if (parseInt(mm) < 1 || parseInt(mm) > 12) return null;
     return `${yyyy}-${mm}`;
   }
   return null;
