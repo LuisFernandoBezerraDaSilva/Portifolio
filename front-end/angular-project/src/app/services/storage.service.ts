856af60c8367e039ba0d84ea2a62f6398b3e0ba5
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-06-26 10:48:04 -0300

    fix: corrigir chave do token no StorageService Angular - accessToken

--- front-end/angular-project/src/app/services/storage.service.ts
+++ front-end/angular-project/src/app/services/storage.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-  private tokenKey: string = 'authToken';
+  private tokenKey: string = 'accessToken';
 
   setToken(token: string): void {
     localStorage.setItem(this.tokenKey, token);
